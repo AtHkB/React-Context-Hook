d069320cef40a2bf16b772ad83160a4a4c79b294
Ata Hakkak <ata.hakkak@gmail.com>
2024-02-16 14:42:24 +0100

    added contexts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,22 @@
-export default function Navbar() {
-  return (
-    <nav style={{ background: "#ddd", color: "#555" }}>
-      <span style={{ cursor: "pointer", float: "left" }}>Log in</span>
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+import { AuthContext } from "../context/AuthContext";
+import ThemeToggle from "./ThemeToggle";
 
-      <span style={{ cursor: "pointer", float: "right" }}>Dark Mode 🌚</span>
+export default function Navbar() {
+  const { dark, light, isLightTheme } = useContext(ThemeContext); //destructure {these light, dark, etc. } from (ThemeContext).
+  const { isAuth, toggleAuth } = useContext(AuthContext);
+
+  const themeStyles = isLightTheme ? light : dark; //Very important step to avoid many ternaries and light.bg or dark.bg
+
+  /* console.log("Theme: ", theme); */
+  return (
+    <nav style={{ background: themeStyles.ui, color: themeStyles.text }}>
+      <span onClick={toggleAuth} style={{ cursor: "pointer", float: "left" }}>
+        {isAuth ? "Log out" : "Log in"}
+      </span>
+
+      <ThemeToggle />
       <h1>Context App</h1>
 
       <br />
